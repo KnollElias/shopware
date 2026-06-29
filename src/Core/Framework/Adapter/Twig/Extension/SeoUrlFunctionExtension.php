5f41f1b396f82c8a330fdfea91852eecd7f30696
D.Neustadt <d.neustadt@shopware.com>
2026-06-29 16:06:50 +0200

    feat: headless product export

--- src/Core/Framework/Adapter/Twig/Extension/SeoUrlFunctionExtension.php
+++ src/Core/Framework/Adapter/Twig/Extension/SeoUrlFunctionExtension.php
@@ -2,7 +2,10 @@
 
 namespace Shopware\Core\Framework\Adapter\Twig\Extension;
 
+use Shopware\Core\Content\Seo\Exception\SeoUrlRouteConfigException;
 use Shopware\Core\Content\Seo\SeoUrlPlaceholderHandlerInterface;
+use Shopware\Core\Content\Seo\SeoUrlRoute\EntityRouteResolver;
+use Shopware\Core\Framework\Adapter\Twig\TwigContextHelper;
 use Shopware\Core\Framework\Log\Package;
 use Symfony\Bridge\Twig\Extension\RoutingExtension;
 use Twig\Extension\AbstractExtension;
@@ -19,22 +22,54 @@ class SeoUrlFunctionExtension extends AbstractExtension
      */
     public function __construct(
         private readonly RoutingExtension $routingExtension,
-        private readonly SeoUrlPlaceholderHandlerInterface $seoUrlReplacer
+        private readonly SeoUrlPlaceholderHandlerInterface $seoUrlReplacer,
+        private readonly EntityRouteResolver $entityRouteResolver,
     ) {
     }
 
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('seoUrl', $this->seoUrl(...), ['is_safe_callback' => $this->routingExtension->isUrlGenerationSafe(...)]),
+            new TwigFunction('seoUrl', $this->seoUrl(...), [
+                'is_safe_callback' => $this->routingExtension->isUrlGenerationSafe(...),
+                'needs_context' => true,
+            ]),
         ];
     }
 
     /**
-     * @param array<string, mixed> $parameters
+     * @param array<string, mixed> $context
+     * @param array<string|int, mixed> $parameters
      */
-    public function seoUrl(string $name, array $parameters = []): string
+    public function seoUrl(array $context, string $name, array $parameters = []): string
     {
-        return $this->seoUrlReplacer->generate($name, $parameters);
+        if (str_contains($name, '.')) {
+            return $this->seoUrlReplacer->generate($name, $parameters);
+        }
+
+        $primaryKey = array_first($parameters);
+        if (!\is_string($primaryKey)) {
+            return $this->seoUrlReplacer->generate($name, $parameters);
+        }
+
+        try {
+            return $this->entityRouteResolver->generateSeoUrlPlaceholder(
+                $name,
+                $primaryKey,
+                $this->isSalesChannelHeadless($context)
+            );
+        } catch (SeoUrlRouteConfigException) {
+            return $this->seoUrlReplacer->generate($name, $parameters);
+        }
+    }
+
+    /**
+     * @param array<string, mixed> $context
+     */
+    private function isSalesChannelHeadless(array $context): bool
+    {
+        $salesChannelContext = TwigContextHelper::getSalesChannelContext($context);
+
+        return $salesChannelContext?->isHeadless() === true;
     }
 }
